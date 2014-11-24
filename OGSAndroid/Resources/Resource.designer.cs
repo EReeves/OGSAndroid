ce77b90bb4f03cb3dd0f08af8c5250b903386c17
Evan Reeves <evanreeves22@gmail.com>
2014-11-24 12:41:39 +1100

    More UI

--- OGSAndroid/Resources/Resource.designer.cs
+++ OGSAndroid/Resources/Resource.designer.cs
@@ -123,13 +123,22 @@ namespace OGSAndroid
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Icon = 2130837504;
+			public const int BlackBorder = 2130837504;
 			
 			// aapt resource value: 0x7f020001
-			public const int monoandroidsplash = 2130837505;
+			public const int GradientDither = 2130837505;
 			
 			// aapt resource value: 0x7f020002
-			public const int woodtex = 2130837506;
+			public const int Icon = 2130837506;
+			
+			// aapt resource value: 0x7f020003
+			public const int monoandroidsplash = 2130837507;
+			
+			// aapt resource value: 0x7f020004
+			public const int WhiteBorder = 2130837508;
+			
+			// aapt resource value: 0x7f020005
+			public const int woodtex = 2130837509;
 			
 			static Drawable()
 			{
